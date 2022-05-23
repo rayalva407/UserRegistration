fcece973454aa4dd98054847374726c979e0b751
rayalva407 <rayalva407@gmail.com>
2022-05-22 23:12:14 -0600

    Creates signup page

--- src/main/java/com/example/UserRegistration/Controller/AppController.java
+++ src/main/java/com/example/UserRegistration/Controller/AppController.java
@@ -1,14 +1,22 @@
 package com.example.UserRegistration.Controller;
 
+import com.example.UserRegistration.Model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class AppController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String viewHomePage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup_form";
     }
 }
