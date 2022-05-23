028d1640b2bd80f991aaa2179cd6dfa6b1092b8c
rayalva407 <rayalva407@gmail.com>
2022-05-23 00:06:00 -0600

    Codes Login and users index page

--- src/main/java/com/example/UserRegistration/Controller/AppController.java
+++ src/main/java/com/example/UserRegistration/Controller/AppController.java
@@ -3,11 +3,14 @@ package com.example.UserRegistration.Controller;
 import com.example.UserRegistration.Model.User;
 import com.example.UserRegistration.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -28,8 +31,19 @@ public class AppController {
 
     @PostMapping("/process_register")
     public String processRegistration(User user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         repo.save(user);
 
         return "register_success";
+    }
+
+    @GetMapping("/list_users")
+    public String viewUsers(Model model) {
+        List<User> listUsers = repo.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
     }
 }
